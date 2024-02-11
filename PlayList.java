2205b1de34b7f0a1275e34c277c917716d4194cc
chenhadad2 <chen.hadad@post.runi.ac.il>
2024-02-11 17:35:15 +0200

    11.02

--- PlayList.java
+++ PlayList.java
@@ -35,33 +35,63 @@ class PlayList {
      *  If the list is full, does nothing and returns false.
      *  Otherwise, appends the track and returns true. */
     public boolean add(Track track) {
-        //// replace the following statement with your code
-        return true;
+            if (size < maxSize) {
+                tracks[size-1] = track;
+            
+            return true;
+            }
+    
+        
+        else {
+            return false;
+
+        }
+        
     }
 
     /** Returns the data of this list, as a string. Each track appears in a separate line. */
     //// For an efficient implementation, use StringBuilder.
     public String toString() {
-        //// replace the following statement with your code
-        return "";
+       String trackstring = "";
+        for (int i = 0; i < size; i++){
+       
+        trackstring += "\n" + tracks[i].getTitle() + "," + tracks[i].getArtist() + "," + tracks[i]. getDuration();
+        }
+        
+        return trackstring;
     }
 
     /** Removes the last track from this list. If the list is empty, does nothing. */
-     public void removeLast() {
-        //// replace this comment with your code
-    }
+    // public void removeLast() {
+     //   if (size > 0){
+       //     tracks[size-1] == {};
+
+      //  }
+   // }
     
     /** Returns the total duration (in seconds) of all the tracks in this list.*/
     public int totalDuration() {
-        //// replace the following statement with your code
-        return 0;
+        int total = 0;
+        for(int i = 0; i < size; i++){
+              
+        total = total + tracks[i].getDuration();
+        }
+        return total;
     }
 
     /** Returns the index of the track with the given title in this list.
      *  If such a track is not found, returns -1. */
     public int indexOf(String title) {
-        //// replace the following statement with your code
-        return -1;
+        int index = 0;
+        for (int i = 0; i < size; i++){
+            if (tracks[i].getTitle() == title) {
+            index = i;
+            }
+            else {
+                index = -1;
+            }
+        }
+        return index;
     }
 
     /** Inserts the given track in index i of this list. For example, if the list is
@@ -71,15 +101,33 @@ class PlayList {
      *  is full, does nothing and returns false. Otherwise, inserts the track and
      *  returns true. */
     public boolean add(int i, Track track) {
-        //// replace the following statement with your code
+
+        if ((size < maxSize) && (i >= 0) && (i < size)) {
+        for (int j = i; j < maxSize; j++){
+            tracks[i] = track;
+            tracks[j] = tracks[j+1];
+        }
+        return true;
+       }
+       else {
         return false;
+       }
+
     }
      
     /** Removes the track in the given index from this list.
      *  If the list is empty, or the given index is negative or too big for this list, 
      *  does nothing and returns -1. */
     public void remove(int i) {
-        //// replace this comment with your code
+        if ((size != 0) && (i > 0) && (i <= size-1)) {
+            for (int j = i; j <= size-i; j++ ){
+                tracks[i+1] = tracks[i];
+                
+            }
+        }
+        else {
+            return;
+        }
     }
 
     /** Removes the first track that has the given title from this list.
